9cc3d1f53ed7112616133334f204275f37554af8
D-RUBY <nkamadavid4@gmail.com>
2023-02-17 05:47:38 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h
+/**
+ * main - This is a program that displays a line using the function puts
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
