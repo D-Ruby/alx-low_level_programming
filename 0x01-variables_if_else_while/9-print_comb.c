723548e939422c9f23e79c6859e677b6eadfb100
D-RUBY <nkamadavid4@gmail.com>
2023-02-19 05:50:57 -0800

    9 print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Print all single number of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar();
+			putchar();
+		}
+		c++;
+	}
+}
